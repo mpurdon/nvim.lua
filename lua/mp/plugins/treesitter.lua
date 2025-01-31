267b4bf1cab90bdd86dbd3af4a1f4d2ed61b09a1
Matthew Purdon <matthew@codenaked.org>
2025-01-31 17:44:23 -0500

    updating treesitter config

--- lua/mp/plugins/treesitter.lua
+++ lua/mp/plugins/treesitter.lua
@@ -1,13 +1,18 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    event = { "BufReadPre", "BufNewFile" },
     build = ':TSUpdate',
     config = function()
-        require('nvim-treesitter.configs').setup({
+        -- import nvim-treesitter plugin
+        local treesitter = require("nvim-treesitter.configs")
+
+        treesitter.setup({
             ensure_installed = {
                 'bash',
                 'c',
                 'cpp',
                 'css',
+                'gitignore',
                 'go',
                 'html',
                 'javascript',
@@ -16,7 +21,10 @@ return {
                 'python',
                 'php',
                 'rust',
-                'typescript'
+                'typescript',
+                'vim',
+                'vimdoc',
+                'yaml'
             },
             sync_install = false,
             auto_install = true,
@@ -26,7 +34,16 @@ return {
             highlight = {
                 enable = true,
                 additional_vim_regex_highlighting = { 'markdown' }
-            }
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
         })
 
         local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
