8ae69c149e7319c7b1f3252eb820e1a5be59c2e1
Matthew Purdon <matthew@codenaked.org>
2025-01-31 17:24:55 -0500

    setting up telescope

--- lua/mp/plugins/telescope.lua
+++ lua/mp/plugins/telescope.lua
@@ -1,29 +1,48 @@
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
+    tag = "0.1.x",
     dependencies = {
-        "plenary"
+        "plenary",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        "nvim-tree/nvim-web-devicons",
     },
     config = function()
-        require('telescope').setup()
+        local telescope = require("telescope")
+        local actions = require("telescope.actions")
         local builtin = require("telescope.builtin")
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
+
+        telescope.setup({
+            defaults = {
+                path_display = { "smart" },
+                mappings = {
+                    i = {
+                        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                        ["<C-j>"] = actions.move_selection_next,     -- move to next result
+                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                    },
+                }
+            },
+        })
+
+        telescope.load_extension("fzf")
+
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Telescope: Find files" })
+        vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "Telescope: Find files with git" })
+        vim.keymap.set('n', '<leader>fs', function()
             builtin.grep_string({
                 search = vim.fn.input("Grep for: ")
             })
-        end, {})
-        vim.keymap.set('n', '<leader>pws', function()
+        end, { desc = "Telescope: Find string in CWD" })
+        vim.keymap.set('n', '<leader>fc', function()
             builtin.grep_string({
                 search = vim.fn.expand("<cword>")
             })
-        end, {})
-        vim.keymap.set('n', '<leader>pWs', function()
+        end, { desc = "Telescope: Grep current word in CWD" })
+        vim.keymap.set('n', '<leader>fC', function()
             builtin.grep_string({
                 search = vim.fn.expand("<cWORD>")
             })
-        end, {})
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        end, { desc = "Telescope: Grep current word matching case in CWD" })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     end
 }
